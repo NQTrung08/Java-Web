5b25a556faae439e8ce773135a71e9f18ed87d07
tiennehihi <vuductientienti@gmail.com>
2024-07-14 22:57:55 +0700

    update userSession

--- src/main/java/com/TLU/studentmanagement/session/UserSession.java
+++ src/main/java/com/TLU/studentmanagement/session/UserSession.java
@@ -4,6 +4,8 @@ import main.java.com.TLU.studentmanagement.model.User;
 
 public class UserSession {
     private static User user;
+    private static String accessToken;
+    private static String refreshToken;
 
     public static User getUser() {
         return user;
@@ -15,6 +17,22 @@ public class UserSession {
 
     public static void clear() {
         user = null;
+    }
+
+    public static String getAccessToken() {
+        return accessToken;
+    }
+
+    public static void setAccessToken(String token) {
+        UserSession.accessToken = token;
+    }
+
+    public static String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public static void setRefreshToken(String token) {
+        UserSession.refreshToken = token;
     }
 }
 
