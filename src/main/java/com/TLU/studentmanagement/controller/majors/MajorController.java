0cdb3b29474d607729ef88d89a64edab74e36f54
NQTrung08 <nqtrung0810@gmail.com>
2024-07-15 12:33:12 +0700

    feat: create controller xử lý major

---
+++ src/main/java/com/TLU/studentmanagement/controller/majors/MajorController.java
@@ -0,0 +1,53 @@
+package main.java.com.TLU.studentmanagement.controller.majors;
+
+import main.java.com.TLU.studentmanagement.model.Major;
+import main.java.com.TLU.studentmanagement.util.HttpUtil;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MajorController {
+
+    public static List<Major> getAllMajors() throws Exception {
+        String apiUrl = "http://localhost:8080/api/major/getAll";
+        String response = HttpUtil.sendGet(apiUrl);
+        JSONObject jsonResponse = new JSONObject(response);
+        JSONArray majorsArray = jsonResponse.getJSONArray("data");
+
+        List<Major> majors = new ArrayList<>();
+        for (int i = 0; i < majorsArray.length(); i++) {
+            JSONObject majorObject = majorsArray.getJSONObject(i);
+            Major major = new Major();
+            major.setId(majorObject.getString("_id"));
+            major.setName(majorObject.getString("name"));
+            major.setCode(majorObject.getString("code"));
+            majors.add(major);
+        }
+        return majors;
+    }
+
+    public static void createMajor(String name, String code) throws Exception {
+        JSONObject jsonInput = new JSONObject();
+        jsonInput.put("name", name);
+        jsonInput.put("code", code);
+
+        String apiUrl = "http://localhost:8080/api/major/create";
+        HttpUtil.sendPost(apiUrl, jsonInput.toString());
+    }
+
+    public static void updateMajor(String id, String name, String code) throws Exception {
+        JSONObject jsonInput = new JSONObject();
+        jsonInput.put("name", name);
+        jsonInput.put("code", code);
+
+        String apiUrl = "http://localhost:8080/api/major/update/" + id;
+        HttpUtil.sendPut(apiUrl, jsonInput.toString());
+    }
+
+    public static void deleteMajor(String id) throws Exception {
+        String apiUrl = "http://localhost:8080/api/major/delete/" + id;
+        HttpUtil.sendDelete(apiUrl);
+    }
+}
